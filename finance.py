c590dd4ac53ba2a280963684e4cd2d0a0ea67c34
dvorkapic <76246307+dvorkapic@users.noreply.github.com>
2026-05-16 18:15:21 +0200

    Add files via upload

---
+++ finance.py
@@ -0,0 +1,9 @@
+def calculate_gross_margin (revenue, cost):
+    margin = revenue - cost
+    print(margin) 
+
+
+
+def apply_tax (amount,rate):
+    tax = amount * rate
+    print(tax)
---
+++ main.py
@@ -0,0 +1,7 @@
+from finance import calculate_gross_margin
+from finance import apply_tax
+
+calculate_gross_margin (2000, 500)
+
+apply_tax(1000, 3)
+
